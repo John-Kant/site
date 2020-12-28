a0db2b2efd09f71eed1ad157c84717cc31ed8c05
bakarytoure <43843959+bakarytoure@users.noreply.github.com>
2020-12-28 20:41:55 +0100

    Add files via upload

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,30 +1,44 @@
-import React from 'react'
-// import $ from 'jquery'
-function Navbar() {
+import React from "react";
+const Navbar = () => {
+  const [height, setHeight] = React.useState(0);
+  const [toggler, setToggler] = React.useState(false);
+  React.useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setHeight(window.pageYOffset);
+    });
 
-    return (
-        <div>
-        <nav className="navbar">
-            <div className="inner-width">
-                <a href="#" className="logo" />
-                <button className="menu-toggler">
-                    <span />
-                    <span />
-                    <span />
-                </button>
-                <div className="navbar-menu">
-                    <a href="#home">Home</a>
-                    <a href="#about">About</a>
-                    <a href="#services">Services</a>
-                    <a href="#education">Education</a>
-                    <a href="#works">Works</a>
-                    <a href="#contact">Contact</a>
-                </div>
-            </div>
-        </nav>
+    return () => window.removeEventListener("scroll", () => {});
+  });
+
+  const handleToggler = () => {
+    setToggler(!toggler);
+  };
+  return (
+    <nav className={height > 20 ? "navbar sticky " : "navbar goTop"}>
+      <div className="inner-width">
+        <a href="#" className="logo" />
+        <button
+          className={toggler ? "active menu-toggler" : "menu-toggler"}
+          onClick={handleToggler}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <div
+          className={toggler ? "active navbar-menu" : "navbar-menu"}
+          onClick={handleToggler}
+        >
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#services">Services</a>
+          <a href="#education">Education</a>
+          <a href="#works">Works</a>
+          <a href="#contact">Contact</a>
         </div>
-
-    )
-}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
