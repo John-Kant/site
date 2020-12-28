04ce62de59a750510090d316fa4ac80ddfc99178
John Gomez <johngomez5541@yahoo.com>
2020-12-28 14:48:48 +0100

    first commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <section id="home">
+            <div className="inner-width">
+                <div className="content">
+                <h1>Hi I'm </h1>
+                <div className="sm">
+                    <a href="#" className="fab fa-facebook-f" />
+                    <a href="#" className="fab fa-twitter" />
+                    <a href="#" className="fab fa-instagram" />
+                    <a href="#" className="fab fa-linkedin-in" />
+                    <a href="#" className="fab fa-youtube" />
+                </div>
+                <div className="buttons">
+                    <a href="#">Contact me</a>
+                    <a href="#">Download CV</a>
+                </div>
+                </div>
+            </div>
+        </section>
+
+    )
+}
