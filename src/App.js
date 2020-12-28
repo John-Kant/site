aa95733223bc25cb3ba0c9cebc19e7e97c3e5a8f
bakarytoure <43843959+bakarytoure@users.noreply.github.com>
2020-12-28 20:54:46 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react"
+import React from "react";
 
 // components
-import Navbar from "./Components/Navbar/Navbar"
-import Home from "./Components/Home/Home"
-import About from "./Components/About/About"
-import Services from "./Components/Services/Services"
-import Contact from "./Components/Contact/Contact"
-import Footer from "./Components/Footer/Footer"
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Services from "./Components/Services/Services";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import BackToTop from "./Components/BackToTopButton.js";
 
 function App() {
   return (
     <>
+      <BackToTop />
       <Navbar />
       <Home />
       <About />
