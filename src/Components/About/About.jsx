e378ec1be40ac5e0ab478a344440f301bb7c66f4
bakarytoure <43843959+bakarytoure@users.noreply.github.com>
2020-12-29 13:48:41 +0100

    Add files via upload

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import about from "../../images/about.png"
 
 export default function About() {
     return (
@@ -7,18 +6,62 @@ export default function About() {
             <div className="inner-width">
                 <h1 className="section-title">About</h1>
                 <div className="about-content">
-                    <img src={about} alt className="about-pic" />
-                    <div className="about-text">
-                        <h2>Hi! I'm Darkcode</h2>
-                        <h3>
-                        <span>Developer</span>
-                        <span>Designer</span>
-                        <span>Youtuber</span>
-                        </h3>
-                        <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed fuga eaque, culpa cupiditate animi laborum natus! Similique odio sit facilis dolorum dicta velit provident sequi iusto, est pariatur quisquam corrupti animi necessitatibus aliquid deserunt reprehenderit voluptatibus? Exercitationem deserunt fugit dicta nesciunt velit quae tempora eius praesentium odio est, voluptatem vero incidunt omnis ad iure, soluta maiores nostrum inventore alias culpa. Nihil suscipit possimus est harum accusamus impedit libero deleniti repellat.
-                        </p>
+                <img src="images/pic.png" alt className="about-pic" />
+                <div className="about-text">
+                    <h2>Hi! I'm Darkcode</h2>
+                    <h3>
+                    <span>Developer</span>
+                    <span>Designer</span>
+                    <span>Youtuber</span>
+                    </h3>
+                    <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed fuga eaque, culpa cupiditate animi laborum natus! Similique odio sit facilis dolorum dicta velit provident sequi iusto, est pariatur quisquam corrupti animi necessitatibus aliquid deserunt reprehenderit voluptatibus? Exercitationem deserunt fugit dicta nesciunt velit quae tempora eius praesentium odio est, voluptatem vero incidunt omnis ad iure, soluta maiores nostrum inventore alias culpa. Nihil suscipit possimus est harum accusamus impedit libero deleniti repellat.
+                    </p>
+                </div>
+                </div>
+                <div className="skills">
+                <div className="skill">
+                    <div className="skill-info">
+                    <span>HTML</span>
+                    <span>90%</span>
                     </div>
+                    <div className="skill-bar html" />
+                </div>
+                <div className="skill">
+                    <div className="skill-info">
+                    <span>CSS</span>
+                    <span>80%</span>
+                    </div>
+                    <div className="skill-bar css" />
+                </div>
+                <div className="skill">
+                    <div className="skill-info">
+                    <span>Javascript</span>
+                    <span>70%</span>
+                    </div>
+                    <div className="skill-bar js" />
+                </div>
+                <div className="skill">
+                    <div className="skill-info">
+                    <span>PHP</span>
+                    <span>60%</span>
+                    </div>
+                    <div className="skill-bar php" />
+                </div>
+                <div className="skill">
+                    <div className="skill-info">
+                    <span>MySQL</span>
+                    <span>90%</span>
+                    </div>
+                    <div className="skill-bar mysql" />
+                </div>
+                <div className="skill">
+                    <div className="skill-info">
+                    <span>C#</span>
+                    <span>80%</span>
+                    </div>
+                    <div className="skill-bar cs" />
+                </div>
                 </div>
             </div>
         </section>
