eb0f5b85233a3e5ed3e2ebad8a25dafc212f6d99
bakarytoure <bakarytoure57@gmail.com>
2020-12-29 13:58:34 +0000

    new commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import React from "react";
 
 // components
 
-import Navbar from "./Components/Navbar/Navbar"
-import Home from "./Components/Home/Home"
-import About from "./Components/About/About"
-import Services from "./Components/Services/Services"
-import Contact from "./Components/Contact/Contact"
-import Footer from "./Components/Footer/Footer"
-import BackToTop from "./Components/BackToTopButton"
+import Navbar from "./Components/English/Navbar/Navbar";
+import Home from "./Components/English/Home/Home";
+import About from "./Components/English/About/About";
+import Services from "./Components/English/Services/Services";
+import Contact from "./Components/English/Contact/Contact";
+import Footer from "./Components/English/Footer/Footer";
+import BackToTop from "./Components/English/BackToTopButton";
+import ProviderComponent from "./Components/ProviderComponent";
 
 function App() {
   return (
     <>
       <BackToTop />
       <Navbar />
-      <Home />
-      <About />
-      <Services />
-      <Contact />
-      <Footer />
     </>
   );
 }
